6e0691c8f7f2e296fa74da4f2c0f88ef2e71aa70
tomas :fuentes <tafuentesc@gmail.com>
2013-09-23 22:00:12 -0300

    Se logra crear y editar polígonos en tiempo real. Falta: - Agregar parámetros del edificio asociado - Agregar botón para guardar el polígono - Mostrar feedback que indique que se puede borrar un nodo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PoiMaps::Application.config.secret_token = 'f35f14f2fc8989b769b03688c3b1076bc2bb1c299a12397fbfa04bfa8de39c8ce1e6bf63f6417a971155cffbc000c2e068975c2be80de37e618254daca033743'
